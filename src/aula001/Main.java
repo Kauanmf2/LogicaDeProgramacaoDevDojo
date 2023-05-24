a78ba6ad6ce3cb2f676d81ab5b04346a14da8840
Kauanmf2 <furtadokauan2@gmail.com>
2023-05-24 20:46:05 -0300

    terceiro commit

--- src/aula001/Main.java
+++ src/aula001/Main.java
@@ -3,7 +3,9 @@ package aula001;
 public class Main {
 
     public static void main(String[] args) {
-        int idade = 17;
-        System.out.println("Eu tenho " + idade + " anos");
+        int idade1 = 30;
+        int idade2 = 15;
+        int diferencaDasIdades = idade1 - idade2;
+        System.out.println("A diferança das idades é " + diferencaDasIdades);
     }
 }
